d06c0b9bfcbdfbea27eb408e49d0cde88f7656e1
Xavier Francisco <xavier.n.francisco@gmail.com>
2018-12-05 12:45:14 +0000

    Specs for p1

--- s01/exercises/h99/test/Spec.hs
+++ s01/exercises/h99/test/Spec.hs
@@ -3,44 +3,33 @@ import Test.Tasty.SmallCheck as SC
 import Test.Tasty.QuickCheck as QC
 import Test.Tasty.HUnit
 
-import Data.List
-import Data.Ord
+import Lib99
 
+main :: IO ()
 main = defaultMain tests
 
 tests :: TestTree
-tests = testGroup "Tests" [properties, unitTests]
+tests = testGroup "Problems" [problem1Spec]
 
-properties :: TestTree
-properties = testGroup "Properties" [scProps, qcProps]
+problem1Spec :: TestTree
+problem1Spec = testGroup "Problem #1" [qcProps, unitTests]
 
-scProps = testGroup "(checked by SmallCheck)"
-  [ SC.testProperty "sort == sort . reverse" $
-      \list -> sort (list :: [Int]) == sort (reverse list)
-  , SC.testProperty "Fermat's little theorem" $
-      \x -> ((x :: Integer)^7 - x) `mod` 7 == 0
-  -- the following property does not hold
-  , SC.testProperty "Fermat's last theorem" $
-      \x y z n ->
-        (n :: Integer) >= 3 SC.==> x^n + y^n /= (z^n :: Integer)
-  ]
-
+qcProps :: TestTree
 qcProps = testGroup "(checked by QuickCheck)"
-  [ QC.testProperty "sort == sort . reverse" $
-      \list -> sort (list :: [Int]) == sort (reverse list)
-  , QC.testProperty "Fermat's little theorem" $
-      \x -> ((x :: Integer)^7 - x) `mod` 7 == 0
-  -- the following property does not hold
-  , QC.testProperty "Fermat's last theorem" $
-      \x y z n ->
-        (n :: Integer) >= 3 QC.==> x^n + y^n /= (z^n :: Integer)
+  [ QC.testProperty "head == (last . reversed)" prop_lastOfReversedIsHead
   ]
 
-unitTests = testGroup "Unit tests"
-  [ testCase "List comparison (different length)" $
-      [1, 2, 3] `compare` [1,2] @?= GT
+unitTests :: TestTree
+unitTests = testGroup "(checked by HUnit)"
+  [ testCase "Given a list return its last element" $
+      (problem1 [1]) @?= (1 :: Int)
 
   -- the following test does not hold
-  , testCase "List comparison (same length)" $
-      [1, 2, 3] `compare` [1,2,2] @?= LT
+  , testCase "Given a singleton list return its element" $
+      (problem1 [1, 2, 3]) @?= (3 :: Int)
   ]
+
+
+prop_lastOfReversedIsHead :: NonEmptyList Int -> Bool
+prop_lastOfReversedIsHead (NonEmpty xs) =
+    (head xs) == (problem1 (reverse xs))
