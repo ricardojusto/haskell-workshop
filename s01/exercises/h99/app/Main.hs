f78874796cef82d9df6b3fc1a1bd35ff1d511ef7
Xavier Francisco <xavier.n.francisco@gmail.com>
2018-12-07 06:27:54 +0000

    Tests-[1..8]

--- s01/exercises/h99/app/Main.hs
+++ s01/exercises/h99/app/Main.hs
@@ -4,6 +4,6 @@ import Lib99
 
 main :: IO ()
 main = do
-  putStrLn (show (problem1 [1, 2, 3]))
-  putStrLn (show (problem1 ['1', '2', '3', '5']))
+  putStrLn (show (myLast [1, 2, 3]))
+  putStrLn (show (myLast ['1', '2', '3', '5']))
 
